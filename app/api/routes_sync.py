eb8b38fee74e5e5d414e851ab67abf91afef643e
Andres-ann <andresdngtt@gmail.com>
2025-10-25 16:38:01 -0300

    docs: ajustar la documentacion swagger

--- app/api/routes_sync.py
+++ app/api/routes_sync.py
@@ -1,12 +1,14 @@
 """
 Rutas para sincronizar datos desde la API de reservas hacia la base local.
 """
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.services.data_collector_service import DataCollectorService
 from app.core.database import SessionLocal
 
 router = APIRouter()
+
 
 # Dependencia para obtener una sesión de la base de datos
 def get_db():
@@ -16,10 +18,31 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/", 
-             tags=["Sync"],
-             summary="Sincronizar datos desde la API de reservas",
-             description="Obtiene los datos de reservas desde el microservicio de reservas y los almacena localmente en la base para análisis.")
+
+@router.post(
+    "/sync",
+    tags=["Sync"],
+    summary="Sincronizar datos desde la API de reservas",
+    description="Obtiene los datos de reservas desde el microservicio de reservas y los almacena localmente en la base para análisis.",
+    responses={
+        200: {
+            "description": "Datos sincronizados correctamente",
+            "content": {
+                "application/json": {
+                    "example": {"status": "ok", "records_imported": 100}
+                }
+            },
+        },
+        204: {"description": "No hay datos nuevos para sincronizar"},
+        401: {"description": "Unauthorized"},
+        500: {"description": "Error: Internal Server Error",
+              "content": {
+                "application/json": {
+                    "example": {"detail":"Error message"}
+                }
+            },},
+    },
+)
 async def sync_data(db: Session = Depends(get_db)):
     """
     Obtiene los datos de reservas desde el microservicio de reservas

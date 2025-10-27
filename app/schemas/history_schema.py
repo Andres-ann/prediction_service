829e5805d2f60a87ddb843db28bfee1f4f0734b4
Andres-ann <andresdngtt@gmail.com>
2025-10-26 22:33:59 -0300

    feat: ajustar esquema de datos para nueva respuesta

--- app/schemas/history_schema.py
+++ app/schemas/history_schema.py
@@ -6,9 +6,20 @@ class ReservationHistory(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     reservation_id = Column(Integer, nullable=False)
-    room = Column(String(100), nullable=False)         
-    people_name = Column(String(150), nullable=False)  
-    articles = Column(Text)
+    
+    # Room
+    room_id = Column(Integer, nullable=False)
+    room_name = Column(String(100), nullable=False)
+    room_capacity = Column(Integer, nullable=False)
+
+    # Person
+    people_id = Column(Integer, nullable=False)
+    people_name = Column(String(150), nullable=False)
+    people_email = Column(String(150), nullable=False)
+
+    # Otros campos
+    expected_people = Column(Integer, nullable=False, default=0)
+    articles = Column(Text)  # podés guardar el JSON serializado como string
     date_hour_start = Column(DateTime, nullable=False)
     date_hour_end = Column(DateTime, nullable=False)
     fetched_at = Column(DateTime, nullable=False)
